81e3dea056da86d248406e3919c7fbd650a0265c
Dale <jd713116@ohio.edu>
2020-01-22 10:16:47 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jd713116_MIS4200.Startup))]
+namespace jd713116_MIS4200
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
