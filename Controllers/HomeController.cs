85c9c4c1860c524eb215d06f096e1ade15f34fe5
Joseph Dale <jd713116@ohio.edu>
2020-01-24 10:33:52 -0500

    More SD 1 Updates

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace jd713116_MIS4200.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Info About Me";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "My Contact Info";
 
             return View();
         }
