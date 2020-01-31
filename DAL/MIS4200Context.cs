7083c88a676aadb4818c0fd0ca590734d6309333
Joseph Dale <jd713116@ohio.edu>
2020-01-31 10:32:10 -0500

    Class 1/31

---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using jd713116_MIS4200.Models;
+
+namespace jd713116_MIS4200.DAL
+{
+    public class MIS4200Context : DbContext
+    {
+        public MIS4200Context() : base("name=DefaultConnection")
+        {
+            // this method is a 'constructor' and is called when a new context is created
+            // the base attribute says which connection string to use
+        }
+        public DbSet<Customer> Customers  { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+
+    }
+}
